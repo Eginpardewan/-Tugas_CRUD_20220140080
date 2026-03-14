189d2c7785c94968c6f30874011034d228e1d02f
Egin Pardewan <eginpardewan@gmail.com>
2026-03-14 16:29:38 +0700

    Menambahkan DTO classes

---
+++ src/main/java/com/deploy/tugas/model/dto/KtpDto.java
@@ -0,0 +1,20 @@
+package com.deploy.tugas.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KtpDto {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
