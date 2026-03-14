af3ce26d13b94ad1bc5bf602111314286377fcdf
Egin Pardewan <eginpardewan@gmail.com>
2026-03-14 16:31:52 +0700

    Menambahkan repository KTP dengan JPA methods

---
+++ src/main/java/com/deploy/tugas/repository/KtpRepository.java
@@ -0,0 +1,12 @@
+package com.deploy.tugas.repository;
+
+import com.deploy.tugas.model.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+    boolean existsByNomorKtp(String nomorKtp);
+}
