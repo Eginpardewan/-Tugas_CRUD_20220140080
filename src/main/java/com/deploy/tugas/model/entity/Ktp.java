a52c631c2b9cc9ac02d52f63c26ab3673f927ba4
Egin Pardewan <eginpardewan@gmail.com>
2026-03-14 16:28:03 +0700

    Menambahkan entity KTP

---
+++ src/main/java/com/deploy/tugas/model/entity/Ktp.java
@@ -0,0 +1,36 @@
+package com.deploy.tugas.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ktp")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nomor_ktp", unique = true, nullable = false, length = 16)
+    private String nomorKtp;
+
+    @Column(name = "nama_lengkap", nullable = false)
+    private String namaLengkap;
+
+    @Column(name = "alamat", nullable = false)
+    private String alamat;
+
+    @Column(name = "tanggal_lahir", nullable = false)
+    private LocalDate tanggalLahir;
+
+    @Column(name = "jenis_kelamin", nullable = false)
+    private String jenisKelamin;
+}
